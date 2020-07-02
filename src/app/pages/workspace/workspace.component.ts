2165360f8e31fc6ea24af7c8c41ca5d388f4add8
sakukk <yangkai12138@gmail.com>
2020-07-02 17:04:52 +0800

    使用webworker

--- src/app/pages/workspace/workspace.component.ts
+++ src/app/pages/workspace/workspace.component.ts
@@ -13,6 +13,18 @@ export class WorkSpaceComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker('./workspace.worker', { type: 'module' });
+      worker.onmessage = ({ data }) => {
+        console.log(`page got message: ${data}`);
+      };
+      worker.postMessage('hello');
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+
     this.map = new maptalks.Map("map", {
       center: [121.4854, 31.2285],
       zoom: 14,
@@ -72,3 +84,4 @@ export class WorkSpaceComponent implements OnInit {
     this.map.zoomOut();
   }
 }
+
