c4559f27bcc7359db20920a2daea0d0610b07834
sakukk <yangkai12138@gmail.com>
2020-07-03 16:07:28 +0800

    1

--- src/app/pages/mxgraph/mxgraph.component.ts
+++ src/app/pages/mxgraph/mxgraph.component.ts
@@ -1,19 +1,27 @@
 import {Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 declare var mxGraph: any;
+declare var mxToolbar: any;
+declare var mxCell: any;
+declare var mxGeometry: any;
+declare var mxUtils: any;
 
 @Component({
   selector: 'mx-graph',
   template: `
-    <div #graphContainer id="graphContainer" 
-      style=" background-image: url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAiIGhlaWdodD0iNDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImdyaWQiIHdpZHRoPSI0MCIgaGVpZ2h0PSI0MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTSAwIDEwIEwgNDAgMTAgTSAxMCAwIEwgMTAgNDAgTSAwIDIwIEwgNDAgMjAgTSAyMCAwIEwgMjAgNDAgTSAwIDMwIEwgNDAgMzAgTSAzMCAwIEwgMzAgNDAiIGZpbGw9Im5vbmUiIHN0cm9rZT0iI2QwZDBkMCIgb3BhY2l0eT0iMC4yIiBzdHJva2Utd2lkdGg9IjEiLz48cGF0aCBkPSJNIDQwIDAgTCAwIDAgMCA0MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZDBkMGQwIiBzdHJva2Utd2lkdGg9IjEiLz48L3BhdHRlcm4+PC9kZWZzPjxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JpZCkiLz48L3N2Zz4=');">
+    <div #toolbar id="toolbar"></div>
+    <img [src]="imgurl" width="100%" height="100%"/>
+    <div #graphContainer id="graphContainer" >
     </div>`
   ,
-  styleUrls: [
-  ]
+  styleUrls: ['./mxgraph.less']
 })
 export class MxgraphComponent implements OnInit, AfterViewInit{
-  @ViewChild('graphContainer') container: ElementRef
-  graph: any
+  @ViewChild('graphContainer') container: ElementRef;
+  @ViewChild('toolbar') toolConainer: ElementRef;
+  graph: any;
+  toolbar: any
+  imgurl: string = 'assets/img/test.png';
+
   constructor() {
   }
 
@@ -22,9 +30,78 @@ export class MxgraphComponent implements OnInit, AfterViewInit{
   ngAfterViewInit(): void {
       const container = this.container.nativeElement;
       this.graph = new mxGraph(container);
+      this.graph.dropenabled = false;
 
       const v1 = this.graph.insertVertex(null, null, 'Hello,', 20, 20, 80, 30);
       const v2 = this.graph.insertVertex(null, null, 'World!', 200, 150, 80, 30);
-		  const e1 = this.graph.insertEdge(null, null, '', v1, v2);
+      const e1 = this.graph.insertEdge(null, null, '', v1, v2);
+      
+      this.toolbar = new mxToolbar(this.toolConainer.nativeElement);
+      
+      this.addVertex('assets/mxgraph/images/maximize.gif', 40, 40, 'shape=cylinder');
+      this.addVertex('assets/mxgraph/images/minimize.gif', 30, 40, 'shape=actor');
+      this.toolbar.addLine();
+
+      var button = mxUtils.button('Create toolbar entry from selection', (evt) => {
+
+        if (!this.graph.isSelectionEmpty()) {
+          
+          // Creates a copy of the selection array to preserve its state
+          var cells = this.graph.getSelectionCells();
+          var bounds = this.graph.getView().getBounds(cells);
+          
+          // Function that is executed when the image is dropped on
+          // the graph. The cell argument points to the cell under
+          // the mousepointer if there is one.
+          var funct = function(graph, evt, cell)
+          {
+            graph.stopEditing(false);
+    
+            var pt = graph.getPointForEvent(evt);
+            var dx = pt.x - bounds.x;
+            var dy = pt.y - bounds.y;
+            
+            graph.setSelectionCells(graph.importCells(cells, dx, dy, cell));
+          }
+          // Creates the image which is used as the drag icon (preview)
+          var img = this.toolbar.addMode(null, 'assets/images/outline.gif', funct);
+          mxUtils.makeDraggable(img, this.graph, funct);
+        }
+      });
+  
+      button.style.position = 'absolute';
+      button.style.left = '2px';
+      button.style.top = '2px';
+      
+      document.body.appendChild(button);
+  }
+
+
+  addVertex(icon, w, h, style) {
+    var vertex = new mxCell(null, new mxGeometry(0, 0, w, h), style);
+    vertex.setVertex(true);
+  
+    this.addToolbarItem(this.graph, toolbar, vertex, icon);
+  }
+
+  addToolbarItem(graph, toolbar, prototype, image) {
+    // Function that is executed when the image is dropped on
+    // the graph. The cell argument points to the cell under
+    // the mousepointer if there is one.
+    var funct = function(graph, evt, cell)
+    {
+      graph.stopEditing(false);
+
+      var pt = graph.getPointForEvent(evt);
+      var vertex = graph.getModel().cloneCell(prototype);
+      vertex.geometry.x = pt.x;
+      vertex.geometry.y = pt.y;
+      
+      graph.setSelectionCells(graph.importCells([vertex], 0, 0, cell));
+    }
+
+    // Creates the image which is used as the drag icon (preview)
+    var img = this.toolbar.addMode(null, image, funct);
+    mxUtils.makeDraggable(img, graph, funct);
   }
 }
